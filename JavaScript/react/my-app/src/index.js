0a57b1adbc496b66d92bbffb13949277f3e0c300
Lucas <153150990+LukasO20@users.noreply.github.com>
2024-07-25 23:41:27 -0300

    Introdução ao React - criação de componentes

---
+++ JavaScript/react/my-app/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import Primeiro from './componentes/Primeiro'
+
+//const element = <h1>Esse código parece HTML mas não é HEHEHEHE</h1>
+ReactDOM.render(<Primeiro/>, document.getElementById('root'))
