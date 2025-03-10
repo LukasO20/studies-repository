fcf55f512eca0547638dc9aa3fda3c48ab85e6da
Lucas Oliveira <153150990+LukasO20@users.noreply.github.com>
2025-03-09 21:19:45 -0300

    Added new components to build structure page
    - Added footer component

--- JavaScript/Angular/frontend/src/app/components/template/footer/footer.component.ts
+++ JavaScript/Angular/frontend/src/app/components/template/footer/footer.component.ts
@@ -2,11 +2,11 @@ import { Component } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
-  selector: 'app-header',
+  selector: 'app-footer',
   imports: [MatToolbarModule],
-  templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.scss'
 })
-export class HeaderComponent {
+export class FooterComponent {
 
 }
