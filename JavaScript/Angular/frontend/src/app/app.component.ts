7b635054d33bc9faea9448da371ee17a7306f8e7
Lucas Oliveira <153150990+LukasO20@users.noreply.github.com>
2025-03-15 13:07:49 -0300

    Added nav component

--- JavaScript/Angular/frontend/src/app/app.component.ts
+++ JavaScript/Angular/frontend/src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/template/header/header.component'
 import { FooterComponent } from './components/template/footer/footer.component'
+import { NavComponent } from './components/template/nav/nav.component'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, NavComponent, FooterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 
 export class AppComponent {
